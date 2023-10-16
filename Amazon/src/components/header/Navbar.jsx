36664fa4a363da311281920a2de49b52787d6a7d
Vinod Chandra <vinodchandra979@gmail.com>
2023-10-16 18:02:38 +0530

    Amazon-Clone start on 16/10/23

---
+++ Amazon/src/components/header/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div className="left">
+                <div className="navlogo">
+                    <img src="./amazon_PNG25.png" alt="" />
+                </div>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
