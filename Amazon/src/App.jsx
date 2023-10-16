b2f00c136d443c29b06b6473f52e449cae61fe75
Vinod Chandra <vinodchandra979@gmail.com>
2023-10-16 21:16:16 +0530

    Navigation and Carousel add

--- Amazon/src/App.jsx
+++ Amazon/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from './components/header/Navbar'
+import Maincomp from './components/home/Maincomp'
+import Newnav from './components/newnavbar/Newnav'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Newnav/>
+      <Maincomp/>
     </>
   )
 }
--- Amazon/src/components/header/Navbar.jsx
+++ Amazon/src/components/header/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
   return (
@@ -8,6 +12,24 @@ const Navbar = () => {
             <div className="left">
                 <div className="navlogo">
                     <img src="./amazon_PNG25.png" alt="" />
+                </div>
+                <div className="nav_searchbar">
+                    <input type="text" name="" id="" />
+                    <div className="search_icon">
+                        <SearchIcon id="search"/>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="nav_btn">
+                        <a href="">signin</a>
+                    </div>
+                    <div className="cart_btn">
+                        <Badge badgeContent={4} color="primary">
+                           <ShoppingCartIcon id="icon"/>
+                        </Badge>
+                        <p>Cart</p>
+                    </div>
+                    <Avatar className='avtar'/>
                 </div>
             </div>
         </nav>
