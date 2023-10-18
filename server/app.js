15f63a66695194adef8dc6e276776fbf1b96758b
Vinod Chandra <vinodchandra979@gmail.com>
2023-10-18 18:48:20 +0530

    bakend start

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
+require('./db/conn')
 const express = require("express");
 const app = express();
+const Products = require("./models/productsSchema");
+const DefaultData = require("./defaultdata")
+
 const port = 8005;
+
+
 
 app.listen(port, ()=>{
     console.log(`server is running on port number ${port}`);
 });
 
+DefaultData();
---
+++ server/defaultdata.js
@@ -0,0 +1,19 @@
+const Products = require("./models/productsSchema");
+const productsdata = require("./constant/productsdata")
+
+const DefaultData = async () => {
+    try {
+      // Delete all existing documents in the "Products" collection
+      await Products.deleteMany({});
+  
+      // Insert data from the "productsdata" array into the "Products" collection
+      const storeData = await Products.insertMany(productsdata);
+  
+      console.log(storeData);
+    } catch (error) {
+      console.log('Error in inserting data', error);
+    }
+  };
+  
+
+module.exports = DefaultData;
